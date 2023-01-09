86a1874f1a7a967935bceab14ccf9a44ce1e3b8a
ussraa <92607242+youssra-ghninou@users.noreply.github.com>
2023-01-09 18:12:45 +0100

    feat: password and port modified

--- src/main/java/com/enimplanner/ConnectionUtil.java
+++ src/main/java/com/enimplanner/ConnectionUtil.java
@@ -5,13 +5,15 @@ import java.sql.DriverManager;
 
 public class ConnectionUtil {
     Connection conn = null;
+
     public static Connection connectdb() {
         try {
             Class.forName("org.postgresql.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:49153/studyenim","postgres", "postgrespw");
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/studyenim", "postgres",
+                    "youssra2020");
             return conn;
         } catch (Exception e) {
-            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
             return null;
         }
     }
