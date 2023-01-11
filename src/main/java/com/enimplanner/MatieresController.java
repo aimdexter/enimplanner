1228e54d076590ae342b7f7ad00d434766ae424e
aimdexter <draimade@gmail.com>
2023-01-11 16:17:54 +0100

    feat: display list of matiere

--- src/main/java/com/enimplanner/MatieresController.java
+++ src/main/java/com/enimplanner/MatieresController.java
@@ -7,9 +7,12 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 import static com.enimplanner.LoginController.loggedInUserId;
 
@@ -30,8 +33,14 @@ public class MatieresController implements Initializable {
     @FXML
     private Label textTotalMat;
 
+    @FXML
+    private ListView<String> textlistMat;
+
+    ObservableList<String> items = FXCollections.observableArrayList();
+
     String sql = "SELECT * FROM etudiant WHERE id_etudiant = \'"+loggedInUserId+"';";
-    String countMatiere = "SELECT COUNT(*) FROM matiere AS countmat where id_etudiant = \'"+loggedInUserId+"';";
+    String countMatiere = "SELECT COUNT(*) FROM matiere where id_etudiant = \'"+loggedInUserId+"';";
+    String listmat = "SELECT * FROM matiere where id_etudiant = \'"+loggedInUserId+"';";
 
 
     @Override
@@ -54,10 +63,25 @@ public class MatieresController implements Initializable {
             preparedStatement = connection.createStatement();
             resultSet = preparedStatement.executeQuery(countMatiere);
             if(resultSet.next()){
-                int countmat = resultSet.getInt("countmat");
+                int countmat = resultSet.getInt("count");
                 textTotalMat.setText(""+countmat);
             }
         } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            preparedStatement = connection.createStatement();
+            resultSet = preparedStatement.executeQuery(listmat);
+            while (resultSet.next()) {
+                textlistMat.setItems(items);
+                String nom_matiere = resultSet.getString("nom_matiere").toUpperCase();
+                String date_matiere = resultSet.getString("date_matiere").toUpperCase();
+                String id_etudiant = resultSet.getString("id_etudiant").toUpperCase();
+                String matieres = nom_matiere +" "+ date_matiere+" "+ id_etudiant ;
+                items.add(matieres);
+            }
+    } catch (SQLException e) {
             e.printStackTrace();
         }
         
